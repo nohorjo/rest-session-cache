e57961c1c243ab818688906838e7a896ec728110
Muhammed Haque <m.haque@livelinktechnology.net>
2018-10-02 14:12:51 +0100

    Added encryption

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const { authenticator } = require('otplib');
 const merge = require('recursive-deep-merge');
+const crypto = require('crypto');
 
 if (!process.env.SECRET) throw "SECRET not set";
+
+const algorithm = 'aes-128-cbc';
+const iv = crypto.createHash('sha1').update(process.env.SECRET).digest('hex').substr(0, 16);
+const password = crypto.createHash('sha256').update(process.env.SECRET).digest('hex').substr(0, 16);
 
 const app = express();
 let sessionCache = {};
@@ -19,11 +24,11 @@ app.use((req, resp, next) => {
 app.use(express.json({limit:'10MB'}));
 
 app.get('/', (req, res) => {
-    resp.send(sessionCache);
+    resp.send(encrypt(JSON.stringify(sessionCache)));
 });
 
-app.get('/:sid', ({param: {sid}}, resp) => {
-    resp.send(sessionCache[sid]);
+app.get('/:sid', ({params: {sid}}, resp) => {
+    resp.send(encrypt(JSON.stringify(sessionCache[sid])));
 });
 
 app.delete('/', (req, resp) => {
@@ -31,12 +36,13 @@ app.delete('/', (req, resp) => {
     resp.sendStatus(201);
 });
 
-app.delete('/:sid', ({param:{sid}}, resp) => {
+app.delete('/:sid', ({params:{sid}}, resp) => {
     delete sessionCache[sid];
     resp.sendStatus(201);
 });
 
-app.post('/:sid', ({param: {sid}, body: next}, resp) => {
+app.post('/:sid', ({params: {sid}, body: {next}}, resp) => {
+    next = JSON.parse(decrypt(next));
     const old = sessionCache[sid];
     if (!old) {
         sessionCache[sid] = next;
@@ -49,4 +55,18 @@ app.post('/:sid', ({param: {sid}, body: next}, resp) => {
 const port = process.env.PORT || 80;
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
+
+function encrypt(text) {
+    var cipher = crypto.createCipheriv(algorithm, password, iv);
+    var crypted = cipher.update(text, 'utf8', 'base64');
+    crypted += cipher.final('base64');
+    return crypted;
+}
+
+function decrypt(text) {
+    var decipher = crypto.createDecipheriv(algorithm, password, iv);
+    var dec = decipher.update(text, 'base64', 'utf8');
+    dec += decipher.final('utf8');
+    return dec;
+}
 
